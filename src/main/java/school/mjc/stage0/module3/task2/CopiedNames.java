886044e0021a3dded2217bd5c9d5ee1515091393
Sandro Suqnidze <sandrosuqnidze2@gmail.com>
2023-09-09 13:48:19 +0400

    var names

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -3,12 +3,16 @@ package school.mjc.stage0.module3.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable = 2000;
+        int variableTwo = 2000;
         int value = 1000;
-        int variable = 4000;
+        int variableThree = 4000;
         int value2 = 3000;
         int variables = 5000;
         variables = 7000;
-        int value = 90000;
+        int valueThree = 90000;
+        int valueFour = 100000;
+        int my_integer = 1000000;
+        int my_var = 1234567;
+        int my_integer_two = 1234567;
     }
 }
